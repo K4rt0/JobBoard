3ced0f6225f5fe148bc85860ee16ee264fb0ad1f
Ngo Thai Hoan <ngothaihoan1103@gmail.com>
2025-03-09 11:09:28 +0700

    error page

--- Frontend/src/routes/routes.tsx
+++ Frontend/src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import MainLayout from '@/layouts/MainLayout'
 import HomePage from '@/pages/HomePage'
 import JobDetailPage from '@/pages/JobDetailPage'
+import ErrorPage from '@/pages/ErrorPage'
 
 const AppRoutes = () => {
     return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/job/:jobId" element={<JobDetailPage />} />
                 </Route>
+                <Route path="*" element={<ErrorPage />} />
             </Routes>
         </Router>
     )
